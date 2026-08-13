ad719877c1db438cfa85ec48287b1db918661ae4
Amir Khodabakhshi <62739801+AmirKhodabakhshi98@users.noreply.github.com>
2026-08-13 07:52:39 +0200

    paintings done

--- src/ETE388/SearchHarder/Paintings.java
+++ src/ETE388/SearchHarder/Paintings.java
@@ -1,8 +1,7 @@
 package ETE388.SearchHarder;
 import Kattis.Kattio;
 
-import java.awt.*;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -10,38 +9,39 @@ public class Paintings {
     Node[] colors;
     HashMap<String, Integer> colorID;
     HashMap<Integer, HashSet<Integer>> forbiddenEdges;
+
     private int visitedTotal = 0;
     int uniquePaths = 0;
-    ArrayList<Integer> bestPath = new ArrayList<>();
+    private int[] bestPath;
+    private int[] currPath;
 
     public Paintings(Node[] colors, HashMap<String, Integer> colorID,
                      HashMap<Integer, HashSet<Integer>> forbiddenEdges) {
         this.colors = colors;
         this.colorID = colorID;
         this.forbiddenEdges = forbiddenEdges;
-     //   System.err.println(colors.length);
-        //solve
+
         for (int i = 0; i < colors.length; i++) {
-            naive(colors[i]);
-            //System.err.println("new start node");
-            /*for (Node color : colors) {
-                color.visited=false;
-            }*/
+            currPath = new int[colors.length];
             visitedTotal = 0;
+            dfs(colors[i]);
         }
     }
 
 
 
+
     //för varje nod besök alla unvisited
-    private void naive(Node n){
-        //System.err.println(n.name);
+    private void dfs(Node n){
         n.visited=true;
+        currPath[visitedTotal] = n.value;
         visitedTotal++;
+
         if (visitedTotal == colors.length) {
             uniquePaths++;
-          //  System.err.println("found, backtracking");
-       //     n.visited=false;
+            if (bestPath == null){
+                bestPath = Arrays.copyOf(currPath, currPath.length);
+            }
             return;
         }
 
@@ -52,29 +52,23 @@ public class Paintings {
             if (next.visited){
                 continue;
             }
-            //next.visited=true;
 
             //forbidden
             if (forbiddenEdges.get(next.value).contains(n.value)
             || forbiddenEdges.get(n.value).contains(next.value)) {
                 continue;
             }
-
-                naive(next);
-                visitedTotal--;
-            //    System.err.println("backtracking");
-                next.visited = false;
-
+            dfs(next);
+            visitedTotal--;
+            next.visited = false;
         }
         n.visited=false;
-
     }
 
     private static class Node {
-        String name; //kan nog bli billigare sen med hashmap/id o ba jmf ID
+        String name;
         int value;
         boolean visited = false;
-        ArrayList<Color> forbiddenEdges = new ArrayList<>();
 
         public Node(String name, int value) {
             this.name = name;
@@ -83,7 +77,6 @@ public class Paintings {
     }
 
 
-    //igen med hashmap för jmf etc efter dum solution klar
     public static void main(String[] args) {
         Kattio io = new Kattio();
 
@@ -97,7 +90,6 @@ public class Paintings {
 
             Node[] colors = new Node[nbrOfColors];
             for (int j = 0; j < nbrOfColors; j++) {
-                //colors[j] = new Node(io.getWord(),nbrOfColors-j-1); //0 indexat
                 colors[j] = new Node(io.getWord(),j); //0 indexat
                 colorID.put(colors[j].name, colors[j].value);
                 forbiddenEdges.put(colors[j].value, new HashSet<>(11)); //12max färger
@@ -112,14 +104,12 @@ public class Paintings {
             }
             Paintings p = new Paintings(colors, colorID, forbiddenEdges);
             io.println(p.uniquePaths);
+            for (int j = 0; j < colors.length; j++) {
+                io.print(colors[p.bestPath[j]].name+" ");
+            }
             io.println();
-
         }
-
         io.flush();
         io.close();
-
-
-
     }
 }
