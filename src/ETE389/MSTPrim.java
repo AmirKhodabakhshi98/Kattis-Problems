e0b17c3b4820f0f693d96e0fc72cc1dc412074fc
Amir Khodabakhshi <62739801+AmirKhodabakhshi98@users.noreply.github.com>
2026-08-14 20:38:37 +0200

    mst prim done

--- src/ETE389/MSTPrim.java
+++ src/ETE389/MSTPrim.java
@@ -4,103 +4,86 @@ import Kattis.Kattio;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.PriorityQueue;
 
 public class MSTPrim {
 
     PriorityQueue<Edge> pq;
     Edge[] edges;
-    HashSet<Edge> mst;
-    //HashSet<Integer> unused;
-   // HashSet<Integer> visited;
+    ArrayList<Edge> mst;
     int totalcost = 0;
     Node[] nodes;
+    int[][] output;
+    int nbrOfNodesVisited = 0;
+
 
     public MSTPrim(Edge[]edges, Node[] nodes) {
-            this.edges = edges;
-            pq = new PriorityQueue<>();
-       // pq.addAll(Arrays.asList(edges));
-      //  unused = new HashSet<>(edges.length);
-        mst = new HashSet<>(edges.length);
-      //  visited = new HashSet<>(edges.length);
+        this.edges = edges;
+        pq = new PriorityQueue<>();
+        mst =  new ArrayList<>();
         this.nodes = nodes;
 
         prim();
 
+        if (nbrOfNodesVisited != nodes.length) {
+            totalcost = -1;
+            return;
+        }
 
-
-            System.out.println(totalcost);
-            for (Edge edge : mst) {
-                System.out.println(edge.from + " " + edge.to + " " + edge.weight);
-            }
+        sortOutput();
 
     }
 
 
-    int total = 0;
+    private void sortOutput(){
+        output = new int[mst.size()][2];
+
+        for (int i = 0; i<output.length; i++){
+            Edge e = mst.removeLast();
+            output[i][0] = e.from;
+            output[i][1] = e.to;
+        }
+
+        for (int i = 0; i<output.length; i++) {
+            if (output[i][0] > output[i][1]) {
+                int temp = output[i][1];
+                output[i][1] = output[i][0];
+                output[i][0] = temp;
+            }
+        }
+
+        Arrays.sort(output, Arrays::compare);
+    }
+
+
     private void prim(){
         Node n = nodes[0];
         n.visited = true;
         pq.addAll(n.edges);
-        total++;
+        nbrOfNodesVisited++;
 
         while (!pq.isEmpty()){
             Edge e = pq.poll();
             if (!nodes[e.from].visited || !nodes[e.to].visited){
-     //           System.err.println(e.from + " " + e.to);
                 if (!nodes[e.from].visited){
                     n =  nodes[e.from];
-
                 }else{
                     n =  nodes[e.to];
                 }
 
                 mst.add(e);
                 totalcost+=e.weight;
-                total++;
+                nbrOfNodesVisited++;
                 n.visited = true;
 
                 for (Edge edge : n.edges) {
-
                     if (!nodes[edge.from].visited || !nodes[edge.to].visited){
-                     //   System.err.println("adding edge " + total);
                         pq.add(edge);
                     }
                 }
             }
-
         }
-
-
     }
-
-
-/*
-    int iterations=  0;
-    private void prim(){
-
-        //pq tom men nt unused -> fanns nt.
-        while (!unused.isEmpty() && !pq.isEmpty()){
-            iterations++;
-            //Edge e = pq.poll();
-            Edge e = pq.peek();
-
-            if (unused.contains(e.from) && visited.contains(e.to)
-            || (unused.contains(e.to) && visited.contains(e.from))
-                ){
-                unused.remove(e.from);
-                unused.remove(e.to);
-                visited.add(e.from);
-                visited.add(e.to);
-                mst.add(e);
-                totalcost += e.weight;
-                pq.poll();
-            }
-        }
-        System.out.println("iterations: " + iterations);
-    }
-*/
 
     private static class Edge implements Comparable<Edge>{
 
@@ -121,12 +104,10 @@ public class MSTPrim {
     private static class Node{
         ArrayList<Edge> edges = new ArrayList<>();
         boolean visited;
-
     }
 
-    static void main() {
+    public static void main(String[] args) {
         Kattio io = new Kattio();
-
 
         while (true) {
             int n = io.getInt();
@@ -150,7 +131,14 @@ public class MSTPrim {
 
 
             MSTPrim mst = new MSTPrim(edges, nodes);
-            //mst print
+            if (mst.totalcost == -1){
+                io.println("Impossible");
+                continue;
+            }
+            io.println(mst.totalcost);
+            for (int[] ints : mst.output) {
+                io.println(ints[0] + " " + ints[1]);
+            }
 
         }
 
