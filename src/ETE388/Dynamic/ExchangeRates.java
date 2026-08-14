dac59e5518bb39a65dea4ce43eaf095812ba1e43
Amir Khodabakhshi <62739801+AmirKhodabakhshi98@users.noreply.github.com>
2026-08-14 08:22:04 +0200

    proof of concept uwu

--- src/ETE388/Dynamic/ExchangeRates.java
+++ src/ETE388/Dynamic/ExchangeRates.java
@@ -1,21 +1,51 @@
 package ETE388.Dynamic;
 import Kattis.Kattio;
 
+//dec format
+
 
 public class ExchangeRates {
-    private double ans;
-    private int savings = 1000;
-    private boolean canada = true; //t/f?
+    private double ans = 1000;
+    private double savings = 1000;
+    private boolean isCAD = true; //t/f?
+    private double[] rates;
+    private double comission = 0.97;
 
     ExchangeRates(double[] rates){
-        ans = rates[rates.length-1];
+        this.rates = rates;
+        ans = naive(savings, isCAD, 0 );
+
     }
 
+
+
+    private double naive(double savings, boolean isCAD, int day){
+        if (day== rates.length){
+            return savings;
+        }
+
+
+        return naive(exchange(savings,rates[day],isCAD),
+                !isCAD,day+1);
+
+
+    }
+
+
+    private double exchange(double savings, double rate, boolean isCAD){
+
+
+        if (isCAD){
+            return (savings/rate)*comission;
+        }
+        return savings*rate*comission;
+    }
 
 
 
     static void main() {
     Kattio io = new Kattio();
+
     int d = io.getInt();
     while (d != 0){
         double[] rates = new double[d];
